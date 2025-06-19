87cf3b9ab387d3f1dcf0a5bef73373a417f0d138
samuelsilva <55461497+Samuca0105@users.noreply.github.com>
2025-06-19 08:40:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const flashcards = [
     {
-        question: "O que é a Constituição Federal?",
-        answer: "É a lei fundamental e suprema do Brasil, que organiza o Estado e garante direitos."
+        question: "No atual sistema financeiro nacional, o mercado bancário está dividido em segmentos; nesse sentido, o segmento private bank trabalha com o grande público, muitos clientes, independentemente de seu nível de renda e patrimônio. ",
+        answer: "Resposta: Errado. Comentário: O conceito de private banking, que nada mais é que serviços bancários e financeiros destinados a clientes privados com grande volume de aplicações financeiras, detentores de grande patrimônio.."
     },
     {
-        question: "O que significa CPU?",
-        answer: "Unidade Central de Processamento."
+        question: "A negociação de um ativo no mercado secundário, seja de renda fixa, seja de renda variável, permite que o seu emissor capte recursos por meio de mercado de capitais.",
+        answer: "Resposta: Errado. Comentário: O emissor capta recursos por meio de mercado de capitais somente no mercado primário. No mercado secundário os ativos são negociados entre investidores.."
     },
     {
-        question: "Qual a capital do Brasil?",
-        answer: "Brasília."
+        question: "Um título de renda fixa indexado à inflação oferece riscos de crédito e de liquidez, mas é isento de risco de mercado por sofrer atualização monetária. ",
+        answer: "Resposta: Errado. Comentário: A afirmação de que títulos indexados à inflação são isentos de risco de mercado é incorreta. Embora a atualização monetária proteja o investidor da perda do poder de compra devido à inflação, esses títulos ainda estão sujeitos a outros riscos de mercado, como: risco de taxas de juros, instabilidade política, crises econômicas etc."
     }
     // Adicione mais flashcards aqui
 ];
