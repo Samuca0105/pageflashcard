a1b5f02942e2bcf14dccfa595ac6f8f6e15ab495
samuelsilva <55461497+Samuca0105@users.noreply.github.com>
2025-06-19 09:25:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -79,18 +79,6 @@ const flashcards = [
         question: " 20)  Qual o objetivo central do Sistema Financeiro Nacional?  ",
         answer: " Promover o desenvolvimento equilibrado da economia, garantindo a estabilidade e eficiência do sistema financeiro."
     },
-    {
-        question: "No atual sistema financeiro nacional, o mercado bancário está dividido em segmentos; nesse sentido, o segmento private bank trabalha com o grande público, muitos clientes, independentemente de seu nível de renda e patrimônio. ",
-        answer: "Resposta: Errado. Comentário: O conceito de private banking, que nada mais é que serviços bancários e financeiros destinados a clientes privados com grande volume de aplicações financeiras, detentores de grande patrimônio.."
-    },
-    {
-        question: "A negociação de um ativo no mercado secundário, seja de renda fixa, seja de renda variável, permite que o seu emissor capte recursos por meio de mercado de capitais.",
-        answer: "Resposta: Errado. Comentário: O emissor capta recursos por meio de mercado de capitais somente no mercado primário. No mercado secundário os ativos são negociados entre investidores.."
-    },
-    {
-        question: "Um título de renda fixa indexado à inflação oferece riscos de crédito e de liquidez, mas é isento de risco de mercado por sofrer atualização monetária. ",
-        answer: "Resposta: Errado. Comentário: A afirmação de que títulos indexados à inflação são isentos de risco de mercado é incorreta. Embora a atualização monetária proteja o investidor da perda do poder de compra devido à inflação, esses títulos ainda estão sujeitos a outros riscos de mercado, como: risco de taxas de juros, instabilidade política, crises econômicas etc."
-    }
     // Adicione mais flashcards aqui
 ];
 
